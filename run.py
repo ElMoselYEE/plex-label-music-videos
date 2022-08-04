6f55925910cb37dbcd3c1810f98c66a9a80b2e37
Blayne Moseley <blayne.moseley@segment.com>
2022-08-04 08:37:37 -0500

    add a bit of maturity to the script

--- run.py
+++ run.py
@@ -1,13 +1,20 @@
 import os
 from plexapi.server import PlexServer
+import logging
 
-print("Logging in...")
+logging.getLogger().setLevel(os.environ.get('LOG_LEVEL', logging.INFO))
+
+logging.info(f"Logging into Plex @ {os.environ.get('PLEX_URL')}")
 plex = PlexServer(os.environ.get('PLEX_URL'), os.environ.get('PLEX_TOKEN'))
-print("Logged in")
+
+all_videos = plex.library.section('Music Videos').all()
+logging.info(f"Iterating over all {len(all_videos)} music videos")
 
 updated = 0
-for video in plex.library.section('Music Videos').all():
-    video.addLabel(video.locations[0].split("/")[4])
-    updated += 1
+for video in all_videos:
+    if len(video.labels) == 0:  # for simplicity, just assume any labels means there's nothing to do
+        playlist = video.locations[0].split("/")[4]  # for simplicity, we don't do any safety checks here
+        video.addLabel(video.locations[0].split("/")[4])
+        updated += 1
 
-print(f"Updated {updated} videos")
+logging.info(f"Updated {updated} music videos")
