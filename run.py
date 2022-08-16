9cb0248df138c8076d2a6a505010b9592b60d4e8
Blayne <bmoseley@gmail.com>
2022-08-16 10:48:47 -0500

    version the forced update before updating tags

--- run.py
+++ run.py
@@ -1,11 +1,16 @@
 import os
 from plexapi.server import PlexServer
 import logging
+import time
 
 logging.getLogger().setLevel(os.environ.get('LOG_LEVEL', logging.INFO))
 
 logging.info(f"Logging into Plex @ {os.environ.get('PLEX_URL')}")
 plex = PlexServer(os.environ.get('PLEX_URL'), os.environ.get('PLEX_TOKEN'))
+
+logging.info(f"Refreshing Library")
+plex.library.section("Music Videos").update()
+time.sleep(30)
 
 all_videos = plex.library.section('Music Videos').all()
 logging.info(f"Iterating over all {len(all_videos)} music videos")
